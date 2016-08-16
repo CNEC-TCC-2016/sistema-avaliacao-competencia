238306d86d6d9bf9f24baa0d4075db25dc637197
Luan Lucas <luan.analise@outlook.com>
2016-08-16 11:46:45 -0300

    prencimento automatico tabelas referentes a competencia

---
+++ src/main/resources/db/migration/201608131534_TB_TIPO_COMPETENCIA_default.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM db_projeto.tb_tipo_competencia
+LIMIT 0, 1000
+
+-- Date: 2016-08-16 10:53
+*/
+INSERT INTO `tb_tipo_competencia` (`ID_TIPO_COMPETENCIA`,`TIPO_COMPETENCIA`) VALUES (1,'COMPORTAMENTAL');
+INSERT INTO `tb_tipo_competencia` (`ID_TIPO_COMPETENCIA`,`TIPO_COMPETENCIA`) VALUES (2,'TÉCNICA');
