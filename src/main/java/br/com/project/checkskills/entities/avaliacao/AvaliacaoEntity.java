24325a99426a79583dd76c89e25a96d480e14c80
Luan Lucas <luan.analise@outlook.com>
2016-08-17 19:17:24 -0300

    tESTE FUNCIONANDO PERSITINDO AVALIACAO COM SUCESSO

--- src/main/java/br/com/project/checkskills/entities/avaliacao/AvaliacaoEntity.java
+++ src/main/java/br/com/project/checkskills/entities/avaliacao/AvaliacaoEntity.java
@@ -8,11 +8,14 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import org.springframework.stereotype.Component;
 
+import br.com.project.checkskills.entities.dadosbasicos.FuncionarioEntity;
 import br.com.project.checkskills.utils.BaseEntity;
 
 @Entity
@@ -28,7 +31,22 @@ public class AvaliacaoEntity extends BaseEntity<Long> {
 	@JoinColumn(name="ID_AVALICAO",referencedColumnName="ID_AVALIACAO")
 	private List<AvaliacaoCompetenciaEntity> avaliacaoCompetencias;
 
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "ASS_FUNCIONARIO_AVALIACAO", joinColumns = 
+	@JoinColumn(name = "ID_AVALIACAO"), inverseJoinColumns = 
+	@JoinColumn(name = "ID_FUNCIONARIO"))
+	private List<FuncionarioEntity> funcionarios;
 	
+	
+	
+	public List<FuncionarioEntity> getFuncionarios() {
+		return funcionarios;
+	}
+
+	public void setFuncionarios(List<FuncionarioEntity> funcionarios) {
+		this.funcionarios = funcionarios;
+	}
+
 	public AvaliacaoEntity() {
 	}
 	
--- src/test/java/br/com/project/checkskills/test/repositories/AvalicaoRepositoryTest.java
+++ src/test/java/br/com/project/checkskills/test/repositories/AvalicaoRepositoryTest.java
@@ -12,9 +12,11 @@ import br.com.project.checkskills.entities.avaliacao.AvaliacaoCompetenciaEntity;
 import br.com.project.checkskills.entities.avaliacao.AvaliacaoEntity;
 import br.com.project.checkskills.entities.dadosbasicos.CompetenciaEntity;
 import br.com.project.checkskills.entities.dadosbasicos.EscalaEntity;
+import br.com.project.checkskills.entities.dadosbasicos.FuncionarioEntity;
 import br.com.project.checkskills.repositories.avaliacao.IAvalicaoRepository;
 import br.com.project.checkskills.repositories.dadosbasicos.ICompetenciaRepository;
 import br.com.project.checkskills.repositories.dadosbasicos.IEscalaRepository;
+import br.com.project.checkskills.repositories.dadosbasicos.IFuncionarioRepository;
 import br.com.project.checkskills.test.utils.AbstractDatabaseTest;
 
 public class AvalicaoRepositoryTest extends AbstractDatabaseTest{
@@ -31,6 +33,9 @@ public class AvalicaoRepositoryTest extends AbstractDatabaseTest{
 	
 	@Inject
 	private IEscalaRepository escalaRepository;
+	
+	@Inject
+	private IFuncionarioRepository funcionarioRepository;
 	
 	@Test
 	public void saveAvaliacaoTest(){
@@ -55,9 +60,23 @@ public class AvalicaoRepositoryTest extends AbstractDatabaseTest{
 		avaliacaoCompetencia.add(a5);
 		avaliacaoCompetencia.add(a6);
 		
+		
+		
+		FuncionarioEntity avaliado = new FuncionarioEntity();
+		avaliado = this.funcionarioRepository.findOne(1L);
+		
+		FuncionarioEntity lider = new FuncionarioEntity();
+		lider = this.funcionarioRepository.findOne(2L);
+		
+		
+		List<FuncionarioEntity>  funcionarios = new ArrayList<>();
+		funcionarios.add(avaliado);
+		funcionarios.add(lider);
+		
 		AvaliacaoEntity entity = new AvaliacaoEntity();
 		entity.setAvaliacaoCompetencias(avaliacaoCompetencia);
-		
+	
+		entity.setFuncionarios(funcionarios);
 		
 		this.avalicaoRepository.save(entity);
 		
