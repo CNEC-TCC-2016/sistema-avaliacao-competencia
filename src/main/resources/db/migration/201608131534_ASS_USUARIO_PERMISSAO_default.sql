9dcf485182aeec4585bb889a368c77ecc2233dd4
Luan Lucas <luan.analise@outlook.com>
2016-08-17 19:15:40 -0300

    ATUALIZAÇÕES DE INSERT (AUTOMATICO)

--- src/main/resources/db/migration/201608131534_ASS_USUARIO_PERMISSAO_default.sql
+++ src/main/resources/db/migration/201608131534_ASS_USUARIO_PERMISSAO_default.sql
@@ -2,19 +2,22 @@
 -- Query: SELECT * FROM db_projeto.ass_usuario_permissao
 LIMIT 0, 1000
 
--- Date: 2016-08-13 16:03
+-- Date: 2016-08-17 19:09
 */
-INSERT INTO `db_projeto`.`ass_usuario_permissao` (`ID_PERMISSAO`, `ID_USUARIO`) VALUES ('1', '1');
-INSERT INTO `db_projeto`.`ass_usuario_permissao` (`ID_PERMISSAO`, `ID_USUARIO`) VALUES ('3', '2');
-INSERT INTO `db_projeto`.`ass_usuario_permissao` (`ID_PERMISSAO`, `ID_USUARIO`) VALUES ('3', '3');
-INSERT INTO `db_projeto`.`ass_usuario_permissao` (`ID_PERMISSAO`, `ID_USUARIO`) VALUES ('1', '4');
-INSERT INTO `db_projeto`.`ass_usuario_permissao` (`ID_PERMISSAO`, `ID_USUARIO`) VALUES ('2', '4');
-INSERT INTO `db_projeto`.`ass_usuario_permissao` (`ID_PERMISSAO`, `ID_USUARIO`) VALUES ('3', '4');
-INSERT INTO `db_projeto`.`ass_usuario_permissao` (`ID_PERMISSAO`, `ID_USUARIO`) VALUES ('1', '5');
-INSERT INTO `db_projeto`.`ass_usuario_permissao` (`ID_PERMISSAO`, `ID_USUARIO`) VALUES ('2', '5');
-INSERT INTO `db_projeto`.`ass_usuario_permissao` (`ID_PERMISSAO`, `ID_USUARIO`) VALUES ('2', '6');
-INSERT INTO `db_projeto`.`ass_usuario_permissao` (`ID_PERMISSAO`, `ID_USUARIO`) VALUES ('3', '7');
-INSERT INTO `db_projeto`.`ass_usuario_permissao` (`ID_PERMISSAO`, `ID_USUARIO`) VALUES ('2', '7');
-INSERT INTO `db_projeto`.`ass_usuario_permissao` (`ID_PERMISSAO`, `ID_USUARIO`) VALUES ('1', '8');
-INSERT INTO `db_projeto`.`ass_usuario_permissao` (`ID_PERMISSAO`, `ID_USUARIO`) VALUES ('2', '9');
-INSERT INTO `db_projeto`.`ass_usuario_permissao` (`ID_PERMISSAO`, `ID_USUARIO`) VALUES ('1', '10');
+INSERT INTO `ass_usuario_permissao` (`ID_PERMISSAO`,`ID_USUARIO`) VALUES (1,1);
+INSERT INTO `ass_usuario_permissao` (`ID_PERMISSAO`,`ID_USUARIO`) VALUES (1,2);
+INSERT INTO `ass_usuario_permissao` (`ID_PERMISSAO`,`ID_USUARIO`) VALUES (2,3);
+INSERT INTO `ass_usuario_permissao` (`ID_PERMISSAO`,`ID_USUARIO`) VALUES (3,3);
+INSERT INTO `ass_usuario_permissao` (`ID_PERMISSAO`,`ID_USUARIO`) VALUES (1,4);
+INSERT INTO `ass_usuario_permissao` (`ID_PERMISSAO`,`ID_USUARIO`) VALUES (2,4);
+INSERT INTO `ass_usuario_permissao` (`ID_PERMISSAO`,`ID_USUARIO`) VALUES (3,4);
+INSERT INTO `ass_usuario_permissao` (`ID_PERMISSAO`,`ID_USUARIO`) VALUES (1,5);
+INSERT INTO `ass_usuario_permissao` (`ID_PERMISSAO`,`ID_USUARIO`) VALUES (3,5);
+INSERT INTO `ass_usuario_permissao` (`ID_PERMISSAO`,`ID_USUARIO`) VALUES (1,6);
+INSERT INTO `ass_usuario_permissao` (`ID_PERMISSAO`,`ID_USUARIO`) VALUES (2,7);
+INSERT INTO `ass_usuario_permissao` (`ID_PERMISSAO`,`ID_USUARIO`) VALUES (3,8);
+INSERT INTO `ass_usuario_permissao` (`ID_PERMISSAO`,`ID_USUARIO`) VALUES (1,9);
+INSERT INTO `ass_usuario_permissao` (`ID_PERMISSAO`,`ID_USUARIO`) VALUES (3,9);
+INSERT INTO `ass_usuario_permissao` (`ID_PERMISSAO`,`ID_USUARIO`) VALUES (1,10);
+INSERT INTO `ass_usuario_permissao` (`ID_PERMISSAO`,`ID_USUARIO`) VALUES (2,10);
+INSERT INTO `ass_usuario_permissao` (`ID_PERMISSAO`,`ID_USUARIO`) VALUES (3,10);
