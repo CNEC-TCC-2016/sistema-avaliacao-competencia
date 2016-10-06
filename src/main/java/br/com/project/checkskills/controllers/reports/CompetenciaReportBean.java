9f82f4d7444af5d985c6dd7a66f487d3c73457b7
luan-analiseinfo <luan@analiseinfo.com.br>
2016-10-06 09:59:00 -0300

    Test convert. InputStream to File

--- src/main/java/br/com/project/checkskills/controllers/reports/CompetenciaReportBean.java
+++ src/main/java/br/com/project/checkskills/controllers/reports/CompetenciaReportBean.java
@@ -1,5 +1,7 @@
 package br.com.project.checkskills.controllers.reports;
 
+import java.io.File;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.HashMap;
 import java.util.List;
@@ -10,15 +12,20 @@ import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.FileUtils;
 
 import br.com.project.checkskills.entities.dadosbasicos.CompetenciaEntity;
 import br.com.project.checkskills.repositories.dadosbasicos.ICompetenciaRepository;
+import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.view.JasperViewer;
 
-@ManagedBean(name = "cargoBean")
+@ManagedBean(name = "competenciaReportBean")
 @ViewScoped
 public class CompetenciaReportBean {
 
@@ -36,6 +43,9 @@ public class CompetenciaReportBean {
 			FacesContext fc = FacesContext.getCurrentInstance();
 			ExternalContext ec = fc.getExternalContext();
 
+			HttpServletResponse response = (HttpServletResponse) fc
+					.getExternalContext().getResponse();
+			
 			ec.responseReset();
 
 			ec.setResponseContentType("application/pdf");
@@ -47,11 +57,20 @@ public class CompetenciaReportBean {
 			// GERAR O RELATORIO E PEGAR O OUTPUSTREAM
 			JRBeanCollectionDataSource jrds = new JRBeanCollectionDataSource(competencias);
 
-			JasperPrint printer = JasperFillManager.fillReport(
-					"C:\\Users\\luana\\JaspersoftWorkspace\\MyReports\\competencia.jasper", getParametros(), jrds);
+//			JasperPrint printer = JasperFillManager.fillReport(
+//					"C:\\Users\\luana\\JaspersoftWorkspace\\MyReports\\competencia.jasper", getParametros(), jrds);
+
+			InputStream fonte =  this.getClass().getClassLoader()
+					.getResourceAsStream("competencia.jasper");
+			
+			JasperPrint printer = JasperFillManager.fillReport(fonte, getParametros(), jrds);
+
+			File targetFile = new File("seuarquivo.pdf");
+			 
+		    FileUtils.copyInputStreamToFile(fonte, targetFile);
 
 			JasperExportManager.exportReportToPdfStream(printer, output);
-
+			fc.renderResponse();
 			fc.responseComplete();
 
 		} catch (Exception ex) {
@@ -69,6 +88,35 @@ public class CompetenciaReportBean {
 		return parametros;
 	}
 
+	
+	 public void exibirRelatorio() {
+	        
+		 // Caminho do .jasper do relatorio
+	        String caminhoRelJasper = "relatorios/competencia/competencia.jasper";
+	        
+	        // Lista com beans
+	        this.competencias = competenciaRepository.findAll();
+	        
+	        // Stream com o .jasper
+	        InputStream relJasper = getClass()
+	                .getResourceAsStream(caminhoRelJasper);
+	       
+	      
+	        JRBeanCollectionDataSource ds = new JRBeanCollectionDataSource(
+	        		competencias);
+	        
+	        // Parametros do relatorios
+	        JasperPrint impressao = null;
+	        try {
+	            impressao = JasperFillManager.fillReport(relJasper, getParametros(), ds);
+	            JasperViewer viewer = new JasperViewer(impressao, true);
+	            viewer.setVisible(true);
+	        } catch (JRException e) {
+	            System.out.println(e.getMessage());
+	        }
+	    }
+	
+	
 	public ICompetenciaRepository getCompetenciaRepository() {
 		return competenciaRepository;
 	}
