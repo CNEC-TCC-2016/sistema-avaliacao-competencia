e8553c96c964e872a59b11372e32a19c8bfdd01b
luan-analiseinfo <luan@analiseinfo.com.br>
2016-10-06 10:51:29 -0300

    DADOS FLAYWAY

---
+++ src/main/resources/db/migration/V201607131431__IMPLANTACAO_FUNCIONARIO_default.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM db_projeto.tb_funcionario
+LIMIT 0, 1000
+
+-- Date: 2016-10-06 10:46
+*/
+INSERT INTO `tb_funcionario` (`ID_FUNCIONARIO`,`nome`,`ID_CARGO`) VALUES (1,'luan',1);
+INSERT INTO `tb_funcionario` (`ID_FUNCIONARIO`,`nome`,`ID_CARGO`) VALUES (3,'karolina',1);
+INSERT INTO `tb_funcionario` (`ID_FUNCIONARIO`,`nome`,`ID_CARGO`) VALUES (4,'JUVENIL',3);
+INSERT INTO `tb_funcionario` (`ID_FUNCIONARIO`,`nome`,`ID_CARGO`) VALUES (5,'GIOVANE OLIVEIRA',1);
+INSERT INTO `tb_funcionario` (`ID_FUNCIONARIO`,`nome`,`ID_CARGO`) VALUES (9,'CELSO',3);
